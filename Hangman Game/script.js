c5be82485cc09a960fe0849e4bc564f6a9b41902
김승환 <dkadowlwh@naver.com>
2021-02-23 23:59:36 +0900

    Add: 게임 실패 알림, 게임 재시작 버튼 구현

--- Hangman Game/script.js
+++ Hangman Game/script.js
@@ -1,6 +1,6 @@
 const wordEl = document.getElementById('word');
 const wrongLettersEl = document.getElementById('wrong-letters');
-const playAgainBtn = document.getElementById('play-again');
+const playAgainBtn = document.getElementById('play-button');
 const popup = document.getElementById('popup-container');
 const notification = document.getElementById('notification-container');
 const finalMessage = document.getElementById('final-message');
@@ -38,7 +38,28 @@ function displayWord() {
 
 // Update the wrong letters
 function updateWrongLettersEl() {
-  console.log('Update wrong');
+  // Display wrong letters
+  wrongLettersEl.innerHTML = `
+    ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+    ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+  `;
+
+  // Display parts
+  figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length;
+
+    if(index < errors) {
+      part.style.display = 'block';
+    } else {
+      part.style.display = 'none';
+    }
+  });
+
+  // Check if lost
+  if(wrongLetters.length === figureParts.length) {
+    finalMessage.innerText = 'Unfortunately you lost. 😥';
+    popup.style.display = 'flex';
+  }
 }
 
 // Show notification
@@ -52,7 +73,6 @@ function showNotification() {
 
 // Keydown letter press
 window.addEventListener('keydown', e => {
-  console.log(e.code);
   if(e.keyCode >= 65 && e.keyCode <= 90) {
     const letter = e.key;
 
@@ -75,5 +95,20 @@ window.addEventListener('keydown', e => {
     }
   }
 });
+
+// Restart game and play again
+playAgainBtn.addEventListener('click', () => {
+  // Empty arrays
+  correctLetters.splice(0);
+  wrongLetters.splice(0);
+
+  selectedWord = words[Math.floor(Math.random() * words.length)];
+
+  displayWord();
+
+  updateWrongLettersEl();
+
+  popup.style.display = 'none';
+})
 
 displayWord();
