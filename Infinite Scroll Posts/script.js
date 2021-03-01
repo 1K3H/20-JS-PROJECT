23185e39831ef29659478b50a0507d9d5ca40c12
김승환 <dkadowlwh@naver.com>
2021-03-01 19:16:00 +0900

    Add: infinite scroll 기능 적용

--- Infinite Scroll Posts/script.js
+++ Infinite Scroll Posts/script.js
@@ -2,7 +2,7 @@ const postsContainer = document.getElementById('posts-container');
 const loading = document.querySelector('.loader');
 const filter = document.getElementById('filter');
 
-let limit = 3;
+let limit = 5;
 let page = 1;
 
 // Fetch posts form API
@@ -10,7 +10,6 @@ async function getPosts() {
   const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`);
 
   const data = await res.json();
-
   return data;
 }
 
@@ -33,5 +32,28 @@ async function showPosts() {
   });
 }
 
+// Show loader & fetch more posts
+function showLoading() {
+  loading.classList.add('show');
+
+  setTimeout(() => {
+    loading.classList.remove('show');
+
+    setTimeout(() => {
+      page++;
+      showPosts();
+    }, 300);
+
+  }, 1000);
+}
+
 // Show initial posts
 showPosts();
+
+window.addEventListener('scroll', () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if(scrollTop + clientHeight >= scrollHeight - 5) {
+    showLoading();
+  }
+});
